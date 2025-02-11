0ce4a13749914cf843a0b7ed672b1fe59c7ceaf3
Liyifei <1970518634@qq.com>
2025-02-11 20:56:01 +0800

    a1

--- web/user/form.py
+++ web/user/form.py
@@ -8,6 +8,14 @@ class SignUpForm(UserCreationForm):
         max_length=254,
         help_text='please input valid email address'
     )
+    full_name = forms.CharField(
+        max_length=100,
+        help_text='Please enter your full name.'
+    )
+    shipping_address = forms.CharField(
+        max_length=255,
+        help_text='Please enter your shipping address.'
+    )
 
     class Meta:
         model = User
