5fe73bc337e1397b7ab035e0df60ccd11ab5984b
Liyifei <1970518634@qq.com>
2025-02-21 03:21:17 +0800

    check a14-a20

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 this is a group project for online shopping web site
 
 super user
-name:test
-psw:123456test
+name:bless
+psw:123456bless
+
+
+the only vendor:
+vendor1
+Seller_01
+
--- web/user/admin.py
+++ web/user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
+
+# 定义一个内联管理类，用于在 User 管理页面中编辑 UserProfile
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'UserProfile'
+
+# 自定义 UserAdmin 类，将 UserProfileInline 添加到 User 的管理页面
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+# 取消默认的 User 注册
+admin.site.unregister(User)
+
+# 使用自定义的 UserAdmin 重新注册 User
+admin.site.register(User, CustomUserAdmin)
