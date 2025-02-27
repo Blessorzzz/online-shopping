b1ed82016961c837265a4e0e84baf03644411f12
JoshBSC <p2204750@mpu.edu.mo>
2025-02-27 18:15:19 +0800

    Finished B2

--- web/shoppingcart/views.py
+++ web/shoppingcart/views.py
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from django.db import transaction, IntegrityError
 =======
 from django.db import transaction,IntegrityError
 >>>>>>> 1be2b38b (perfect b4)
+=======
+from django.db import transaction, IntegrityError
+>>>>>>> 8d8815b8 (Finished B2)
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
