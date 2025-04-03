eb8bc172bf2a9cb39e4449810423ae5db8f27a8d
JoshBSC <p2204750@mpu.edu.mo>
2025-04-03 23:06:48 +0800

    Restricts Allowable Commenters Verified Purchase

--- web/forums/views.py
+++ web/forums/views.py
@@ -5,7 +5,7 @@ from .forms import ForumPostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from ecommerce.models import Product  # Import Product model
 from django.db.models import Q
-
+from shoppingcart.models import Order
 @login_required
 def forum_list(request, product_id):
     # Get the product by product_id
@@ -32,18 +32,26 @@ def forum_list(request, product_id):
 
 @login_required
 def forum_detail(request, product_id, post_id):
-    # Get the product and forum post
     product = get_object_or_404(Product, product_id=product_id)
-    post = get_object_or_404(ForumPost, id=post_id, product=product)  # Ensure the post belongs to the product
+    post = get_object_or_404(ForumPost, id=post_id, product=product)
     comments = post.comments.filter(parent_comment__isnull=True).order_by('created_at')
     comment_form = CommentForm()
-    
+
+    # Check if the user has purchased the product
+    user_has_purchased = Order.objects.filter(
+        customer=request.user,
+        status__in=['complete', 'refunded', 'delivered'],
+        items__product=post.product
+    ).exists()
+
     return render(request, 'forums/forum_detail.html', {
         'product': product,
         'post': post,
         'comments': comments,
         'comment_form': comment_form,
+        'user_has_purchased': user_has_purchased,
     })
+
 
 @login_required
 def create_forum_post(request, product_id):
@@ -70,6 +78,18 @@ def create_forum_post(request, product_id):
 @login_required
 def add_comment(request, product_id, post_id):
     post = get_object_or_404(ForumPost, id=post_id, product__product_id=product_id)
+
+    # Check if the user has purchased the product
+    has_purchased = Order.objects.filter(
+        customer=request.user,
+        status__in=['complete', 'refunded', 'delivered'],
+        items__product=post.product
+    ).exists()
+
+    if not has_purchased:
+        # Redirect with an error message if the user has not purchased the product
+        return redirect('forum_detail', product_id=product_id, post_id=post_id)
+
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -77,7 +97,7 @@ def add_comment(request, product_id, post_id):
             comment.author = request.user
             comment.forum_post = post
             comment.save()
-            # Redirect to the forum_detail page with both product_id and post_id
             return redirect('forum_detail', product_id=product_id, post_id=post_id)
-    # Redirect back to the forum_detail page even if the form is invalid
+
     return redirect('forum_detail', product_id=product_id, post_id=post_id)
+
