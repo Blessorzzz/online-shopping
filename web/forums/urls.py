569c0a9eee5727b0441f9c80030fc36cdf00416a
willcih <p2204954@mpu.edu.mo>
2025-04-04 12:32:14 +0800

    Minor fixes

--- web/forums/urls.py
+++ web/forums/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('<uuid:product_id>/', forum_list, name='forum_list'),
     path('<uuid:product_id>/<uuid:post_id>/', forum_detail, name='forum_detail'),
     path('create/<uuid:product_id>/', create_forum_post, name='create_forum_post'),
-    path('<uuid:post_id>/comment/', add_comment, name='add_comment'),
+    path('<uuid:product_id>/<uuid:post_id>/comment/', views.add_comment, name='add_comment'),
     path('vote_comment/', views.vote_comment, name='vote_comment'),
     path('report_comment/<int:comment_id>/', views.report_comment, name='report_comment'),
 ]
